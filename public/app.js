e4ea0efface94ed6535f8a8985378cd2c94064ae
posocco <ari.posocco@gmail.com>
2026-01-07 20:27:17 +0000

    auto update

--- public/app.js
+++ public/app.js
@@ -28,7 +28,7 @@ import {
   orderBy,
   limit,
   onSnapshot
-  chat-social-features
+  chat-social-CSSFontFeatureValuesRule.js
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 let activeLeagueId = null;
